93f56fd8f3fd04ca62f691bd959994c3d1c2eb15
buithihatien2711 <buithihatien2711@gmail.com>
2021-08-05 09:37:06 +0700

    finish-project

---
+++ DAL_ST/Connect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopManagement
+{
+    class Connect
+    {
+        string constr; 
+        public Connect()
+        {
+            constr = @"Data Source=LAPTOP-J27Q18HQ\HATIEN;Initial Catalog=PBL3;Integrated Security=True";
+        }
+        public SqlConnection getConnect()
+        {
+            return new SqlConnection(constr);
+        }
+    }
+}
